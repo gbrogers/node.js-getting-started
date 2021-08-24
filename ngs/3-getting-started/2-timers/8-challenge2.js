e9e286423b81df59a67c1172cddcb11e3446f9fe
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-08-24 14:58:13 -0600

    end of day

---
+++ ngs/3-getting-started/2-timers/8-challenge2.js
@@ -0,0 +1,15 @@
+// Print "Hello World"
+// Every second
+// And stop after 5 times
+let counter = 0;
+const intervalId = setInterval(() => {
+  console.log("Hello World");
+  counter += 1;
+
+  if (counter === 5) {
+    console.log("Done");
+    clearInterval(intervalId);
+  }
+}, 1000);
+
+// After 5 times. Print "Done" and let Node exit.
